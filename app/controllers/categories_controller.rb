13805cf0a74c95450d006b20653244683d65fc5b
Victoria B <viktorija.bourassa@gmail.com>
2019-04-23 17:39:43 +0000

    Automatically backed up by Learn

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,4 +25,7 @@ class CategoriesController < ApplicationController
     category.update(params.require(:category))
     redirect_to category_path(category)
   end
+
+  def category_params
+  end
 end
